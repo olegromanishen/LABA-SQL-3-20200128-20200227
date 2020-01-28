7fc1878d70ccb1e3aef2a99372ce5a753bdc1394
Maksym <belkomaksy@gmail.com>
2020-01-28 14:47:58 +0200

    added data for 1st lecture

---
+++ 01class.sql
@@ -0,0 +1,31 @@
+/*
+ * ######################################################
+ * #     Занятие 1                                      #
+ * #     Основы работы с реляционными базами данных     #
+ * ######################################################
+ */
+
+
+ /*
+  * +------------------+
+  * | Как работает SQL |
+  * +------------------+
+  */
+
+ SELECT * FROM countries;
+
+/*
+ * +-------------+
+ * | Типы данных |
+ * +-------------+
+ */
+
+/*
+Текстовые
+Числовые
+Дата
+
+MS SQL SERVER https://docs.microsoft.com/ru-ru/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-2017
+Oracle https://docs.oracle.com/cd/B28359_01/server.111/b28318/datatype.htm#CNCPT1837
+MySQL https://dev.mysql.com/doc/refman/8.0/en/data-types.html
+*/

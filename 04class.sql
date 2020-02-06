bb5e4f363fd028496235e19e0f31709a185fd09f
Maksym <belkomaksy@gmail.com>
2020-02-06 18:53:44 +0200

    After lesson 4

--- 04class.sql
+++ 04class.sql
@@ -20,18 +20,19 @@
 	prod.list_price, prod.standard_cost*/
 FROM
 	db_laba.dbo.products prod
-ORDER BY 2 DESC, 1;
+--ORDER BY 2 DESC, 1
 --ORDER BY prod.list_price - prod.standard_cost DESC, 1;
 --ORDER BY prod.list_price - prod.standard_cost DESC, prod.product_name;
+ORDER BY margin DESC, prod.product_name;
 
 -- вывести имя продукта и процент маржи по стоимости (2мя способами)
 -- отсортировать по имени продукта по возростанию
 SELECT
 	prod.product_name as name,
 	--prod.list_price , prod.standard_cost,
-   (prod.list_price - prod.standard_cost) / prod.standard_cost * 100 as margin_to_cost
+   --(prod.list_price - prod.standard_cost) / prod.standard_cost * 100 as margin_to_cost
    --,(prod.list_price / prod.standard_cost -1)  * 100 as margin_to_cost_2
-   --,CAST(CAST((prod.list_price / prod.standard_cost -1)  * 100 as decimal(10,2) ) as varchar) + '%' as margin_to_cost_3_proc
+   CAST(CAST((prod.list_price / prod.standard_cost -1)  * 100 as decimal(10,2) ) as varchar) + '%' as margin_to_cost_3_proc
 FROM
 	db_laba.dbo.products prod
 order by
@@ -47,26 +48,29 @@ order by
 -- отсортировать по убыванию маржи и имени продукта по возростанию
  SELECT
 	'For product: ' + prod.product_name as name,
-	prod.list_price - prod.standard_cost as margin,
-	'USD' as CURENCY
+	prod.list_price - prod.standard_cost as margin--,
+	--'USD' as CURENCY
 FROM
 	db_laba.dbo.products prod
 ORDER BY
 	2 DESC,
 	1; 
+--For product: Intel SSDPECME040T401
 
 /*
  * +-------------------+
  * | Строковые функции |
  * +-------------------+
  */
+--Bailey
 SELECT
 	emp.last_name
+	--,LEN(emp.last_name) -- Oracle length
 	--,LEFT ( emp.last_name, 2 ) 
 	--,RIGHT ( emp.last_name, 3 )
 	--,SUBSTRING ( emp.last_name , 1 ,3 ) 
 	--,SUBSTRING ( emp.last_name , 3 ,3 )
-	--,LEN(emp.last_name) 
+	--,reverse(SUBSTRING ( reverse (emp.last_name) , 1 ,3 ))
 	--,LTRIM (emp.last_name)
 	--,RTRIM (emp.last_name)
 	--,TRIM (emp.last_name)
@@ -78,6 +82,7 @@ SELECT
 from db_laba.dbo.employees emp
 where LEN(emp.last_name) > 7; 
 --Rivera
+--'Richardson'
 
 select  LTRIM (' last name   '),
 		RTRIM (' last name   '),
@@ -85,7 +90,8 @@ select  LTRIM (' last name   '),
 --'last name   '
 --' last name'
 --'last name'
-
+--как искать например в 8-м символе определенное значение?
+select SUBSTRING('ry34567890', 8, 1) 
 /*
  * +--------+
  * |  INNER |
@@ -95,6 +101,7 @@ select  LTRIM (' last name   '),
  select
 	*
 	--p.*, pc.*
+	--p.*, pc.category_name 
 from
 	db_laba.dbo.products p,
 	db_laba.dbo.product_categories pc
@@ -103,6 +110,7 @@ where
 
 select
 	*
+	--p.description , pc.category_name 
 from
 	db_laba.dbo.products p
 inner join db_laba.dbo.product_categories pc on
@@ -111,12 +119,12 @@ inner join db_laba.dbo.product_categories pc on
 -- вывести имя продукта, стандартую цену и имя категории
 -- для всех категорий кроме CPU
 -- отсортировать по имения продукта
- select
-	p.product_name,
-	p.standard_cost,
-	--p.category_id category_id_prod,
-	--pc.category_id category_id_cat,
-	pc.category_name
+ select --*
+ 	p.product_name,
+ 	p.standard_cost,
+ 	pc.category_name 
+	--,p.category_id category_id_prod,
+	--pc.category_id category_id_cat
 from
 	db_laba.dbo.products p
 inner join db_laba.dbo.product_categories pc on
@@ -139,13 +147,13 @@ order by 1;
 	emp.first_name,
 	emp.last_name,
 	--ord.order_id,
-	--ord.status
-	--emp.job_title
+	--ord.status,
+	--emp.job_title,
 	COUNT(ord.order_id)
 from
 	db_laba.dbo.employees emp
-left join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
---left outer join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
+--left join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
+left outer join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
  --join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
 where
 	emp.job_title LIKE '%sal%'
@@ -165,9 +173,12 @@ select * from
 	db_laba.dbo.employees emp1;
 SELECT
 	emp1.first_name + ' ' + emp1.last_name as employee,
+	--emp1.employee_id , emp1.manager_id ,
 	--'managed by ',
 	emp2.first_name + ' ' + emp2.last_name as manager ,
-	COALESCE(emp2.first_name + ' ' + emp2.last_name, 'Tommy Bailey') as manager_2
+	--emp2.employee_id , emp2.manager_id ,
+	COALESCE(emp2.first_name + ' ' + emp2.last_name, 'Tommy Bailey') as manager_2,
+	COALESCE(emp2.first_name + ' ' + emp2.last_name, emp1.first_name + ' ' + emp1.last_name) as manager_3
 from
 	db_laba.dbo.employees emp1
 left join db_laba.dbo.employees emp2 on
@@ -193,3 +204,6 @@ FULL OUTER join db_laba.dbo.orders ord on
 	emp.employee_id = ord.salesman_id
 where
 	emp.job_title LIKE '%sal%';
+
+-- * HM
+--select 'moloko'
